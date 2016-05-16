03babeb95187cbfb5b5799c22d20b6c4dc745ab6
PascalRD <rootpascal@gmail.com>
2016-05-16 08:51:41 +0300

    Fix grep_digit

--- src/common.c
+++ src/common.c
@@ -180,10 +180,6 @@ void grep_digits(char *dst, const char *src, size_t len)
         {
             *(dst++) = *src;
         }
-        else
-        {
-            continue;
-        }
     }
 
     *dst = '\0';
