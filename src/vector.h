5e5d91cbd8cfe693276f2e203099ec6accc79a56
PascalRD <rootpascal@gmail.com>
2016-08-14 19:16:33 +0300

    Add vector_search definition

--- src/vector.h
+++ src/vector.h
@@ -7,5 +7,6 @@ const process_data_t *vector_at(const vector_process_t *v, size_t nmemb);
 void vector_free(vector_process_t *v);
 void vector_clear(vector_process_t *v);
 void vector_sort(vector_process_t *v, int flags);
+const process_data_t *vector_search(vector_process_t *v, const char *name);
 
 #endif /* VECTOR_H_ */
